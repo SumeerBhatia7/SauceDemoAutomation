8b3113df7fd021a38a09dd59c31c3c7a695a4df8
sumeerbhatia <bhatiasumeer@gmail.com>
2025-12-17 19:26:35 +0530

    cartpage class

--- src/main/java/pages/CartPage.java
+++ src/main/java/pages/CartPage.java
@@ -1,4 +1,38 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class CartPage {
+
+    WebDriver driver;
+
+    public CartPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // Locators
+    By cartItemName = By.className("inventory_item_name");
+    By checkoutBtn = By.id("checkout");
+    By removeBtn = By.id("remove-sauce-labs-backpack");
+    By cartBadge = By.className("shopping_cart_badge");
+ //   By removeBtn2 = By.id("remove-sauce-labs-fleece-jacket");
+
+    public String getCartItemName() {
+        return driver.findElement(cartItemName).getText();
+    }
+
+    // Click checkout button
+    public void clickCheckout() {
+        driver.findElement(checkoutBtn).click();
+    }
+
+    // Optional: remove product from cart
+    public void removeProduct(){
+        driver.findElement(removeBtn).click();
+    }
+    public boolean isProductRemoved() {
+        return driver.findElements(cartItemName).size() == 0;
+    }
+
 }
--- src/test/java/tests/CheckOutTest.java
+++ src/test/java/tests/CheckOutTest.java
@@ -3,6 +3,7 @@ package tests;
 import base.BaseTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.CartPage;
 import pages.CheckOutPage;
 import pages.HomePage;
 import pages.LoginPage;
@@ -23,7 +24,28 @@ public class CheckOutTest extends BaseTest {
         home.addFirstProductToCart();
         home.goToCart();
 
-        // 3. CHECKOUT STEPS
+        //CART PAGE
+        CartPage cart = new CartPage(driver);
+
+        //// Verify product name
+        String productName = cart.getCartItemName();
+        Assert.assertEquals(productName, "Sauce Labs Backpack");
+
+//        cart.removeProduct();
+//        Assert.assertTrue(cart.isCartEmpty(),
+//                "❌ Cart empty nahi hai");
+
+        cart.removeProduct();
+
+        if (cart.isProductRemoved()) {
+            System.out.println("✅ PRODUCT SUCCESSFULLY REMOVED FROM CART");
+        } else {
+            System.out.println("❌ PRODUCT STILL PRESENT IN CART");
+        }
+
+
+
+     /*   // 3. CHECKOUT STEPS
         CheckOutPage checkout = new CheckOutPage(driver);
 
         checkout.clickCheckout(); // checkout button
@@ -38,6 +60,6 @@ public class CheckOutTest extends BaseTest {
         Assert.assertEquals(message, "Thank you for your order!",
                 "Order not completed!");
 
-        System.out.println("Order Completed Successfully: " + message);
+        System.out.println("Order Completed Successfully: " + message);*/
     }
 }
